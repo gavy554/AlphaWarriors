6e3b8101b91c2d6afbfd6787dbda48247ad30749
Patel Parv <patparvm@sheridancollege.ca>
2024-03-06 20:05:37 -0500

    2nd time COMMITTED

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -8,8 +8,8 @@ package ca.sheridancollege.project;
 /**
  * A class that models each Player in the game. Players have an identifier, which should be unique.
  *
- * @author dancye
- * @author Paul Bonenfant Jan 2020
+ * @author Group9
+ * @author Parv, Daksh, Sudin, Jaiman
  */
 public abstract class Player {
 
